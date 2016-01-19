3433d245f647be56cd5ed1c4d44ed593dc3e89b6
yangxuewu <yxw_android@163.com>
2016-01-19 15:42:22 +0800

    android新特性

---
+++ ToolBar5/androidnewfeature/src/main/java/fm/mow/com/androidnewfeature/frament/MyFragment1.java
@@ -0,0 +1,27 @@
+package fm.mow.com.androidnewfeature.frament;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import fm.mow.com.androidnewfeature.R;
+
+/**
+ * Created by Jay on 2015/8/28 0028.
+ */
+public class MyFragment1 extends Fragment {
+
+    public MyFragment1() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content, container, false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText("1fragment");
+        return view;
+    }
+}
---
+++ ToolBar5/androidnewfeature/src/main/java/fm/mow/com/androidnewfeature/frament/MyFragmentPagerAdapter.java
@@ -0,0 +1,73 @@
+package fm.mow.com.androidnewfeature.frament;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import fm.mow.com.androidnewfeature.TabLayoutFramentActivity;
+
+/**
+ * Created by Jay on 2015/8/31 0031.
+ */
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private MyFragment1 myFragment1 = null;
+    private MyFragment2 myFragment2 = null;
+    private MyFragment3 myFragment3 = null;
+    private MyFragment4 myFragment4 = null;
+    String[] mTitle;
+
+    public MyFragmentPagerAdapter(FragmentManager fm ,String[] mTitle ) {
+        super(fm);
+        this.mTitle=mTitle;
+        myFragment1 = new MyFragment1();
+        myFragment2 = new MyFragment2();
+        myFragment3 = new MyFragment3();
+        myFragment4 = new MyFragment4();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitle[position];
+    }
+
+    @Override
+    public int getCount() {
+        return mTitle.length;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup vg, int position) {
+        return super.instantiateItem(vg, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        System.out.println("position Destory" + position);
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case TabLayoutFramentActivity.PAGE_ONE:
+                fragment = myFragment1;
+                break;
+            case TabLayoutFramentActivity.PAGE_TWO:
+                fragment = myFragment2;
+                break;
+            case TabLayoutFramentActivity.PAGE_THREE:
+                fragment = myFragment3;
+                break;
+            case TabLayoutFramentActivity.PAGE_FOUR:
+                fragment = myFragment4;
+                break;
+        }
+        return fragment;
+    }
+
+
+}
+

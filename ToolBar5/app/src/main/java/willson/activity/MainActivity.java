590ad608b6c14893b49bd303cb5c8ad20d9733ea
xuewu.yang <xuewu.yang@leediancn.com>
2018-07-05 14:22:11 +0800

    update

--- ToolBar5/app/src/main/java/willson/activity/MainActivity.java
+++ ToolBar5/app/src/main/java/willson/activity/MainActivity.java
@@ -3,6 +3,8 @@ package willson.activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,17 +21,20 @@ import willson.fragment.FragmentTwo;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //设置ToolBar
         final Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
         mToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(mToolbar);
 
         //设置抽屉DrawerLayout
-        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.simple_navigation_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.simple_navigation_drawer);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
                 R.string.open, R.string.close);
         mDrawerToggle.syncState();//初始化状态
@@ -37,28 +42,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //设置导航栏NavigationView的点击事件
         NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.item_one:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
-                        mToolbar.setTitle("我的动态");
-                        break;
-                    case R.id.item_two:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentTwo()).commit();
-                        mToolbar.setTitle("我的留言");
-                        break;
-                    case R.id.item_three:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentThree()).commit();
-                        mToolbar.setTitle("附近的人");
-                        break;
-                }
-                menuItem.setChecked(true);//点击了把它设为选中状态
-                mDrawerLayout.closeDrawers();//关闭抽屉
-                return true;
-            }
-        });
+
+        setupDrawerContent(mNavigationView);
+
+//        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                switch (menuItem.getItemId()) {
+//                    case R.id.item_one:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
+//                        mToolbar.setTitle("我的动态");
+//                        break;
+//                    case R.id.item_two:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentTwo()).commit();
+//                        mToolbar.setTitle("我的留言");
+//                        break;
+//                    case R.id.item_three:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentThree()).commit();
+//                        mToolbar.setTitle("附近的人");
+//                        break;
+//                }
+//                menuItem.setChecked(true);//点击了把它设为选中状态
+//                mDrawerLayout.closeDrawers();//关闭抽屉
+//                return true;
+//            }
+//        });
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new FragmentOne()).commit();
+//        mToolbar.setTitle("我的动态");
+        setTitle("我的动态");
+    }
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.item_one:
+                fragmentClass = FragmentOne.class;
+                break;
+            case R.id.item_two:
+                fragmentClass = FragmentTwo.class;
+                break;
+            case R.id.item_three:
+                fragmentClass = FragmentThree.class;
+                break;
+            default:
+                fragmentClass = FragmentOne.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
     }
 
     @Override
--- ToolBar5/app/src/main/java/willson/fragment/FragmentOne.java
+++ ToolBar5/app/src/main/java/willson/fragment/FragmentOne.java
@@ -3,6 +3,7 @@ package willson.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,13 +21,19 @@ import fm.mow.com.toolbar.R;
 public class FragmentOne extends Fragment {
 
 
+    private static final String TAG = FragmentOne.class.getName();
+    private  static  int i =0;
     public FragmentOne() {
+
+        Log.d(TAG, "FragmentOne: "+i);
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Log.d(TAG, "FragmentOne: "+i);
         return inflater.inflate(R.layout.layout_one,container,false);
     }
 
